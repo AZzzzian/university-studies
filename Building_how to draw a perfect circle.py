71c8c9e1f6b2ab74192378404ba194bf9caecb3b
Zian <zirui.zhao3@ucdconnect.ie>
2026-03-19 19:06:59 +0800

    Fix circle drawing by adjusting aspect ratio
    Updated the draw_circle function to adjust for aspect ratio, improving the circle's shape representation.

--- Building_how to draw a perfect circle.py
+++ Building_how to draw a perfect circle.py
@@ -39,16 +39,17 @@ def calculate_wide_and_length(radius):
 
 def draw_circle(radius):
     width,length = calculate_wide_and_length(radius)
-    rex=radius*0.09**2
+    aspect_ratio=1.5
     for y in range(length*2+1):
-        for x in range(-6,width*2+1):
-            distance = math.sqrt(math.pow((y - radius), 2) + math.pow((x - radius)*0.7, 2))
-            if (distance > radius - 0.08*radius and distance < radius + 0.08*radius):
-                print("x", end=" ")
-            elif x == radius and y == radius:
-                print("#", end=" ")
-            else:
-                print(" ", end=" ")
+        for x in range(width*2+):
+               adjusted_x=x / aspect_ratio
+               distance = math.sqrt(math.pow(y - radius, 2) + math.pow(adjusted_x-radius, 2))
+               if (distance > radius - 0.08*radius and distance < radius + 0.08*radius):
+                      print("x", end=" ")
+               elif x == radius and y == radius:
+                      print("#", end=" ")
+               else:
+                      print(" ", end=" ")
 
         print()
 draw_circle(5)
